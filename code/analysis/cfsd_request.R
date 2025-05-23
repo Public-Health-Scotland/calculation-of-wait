fecdbaaedbad49af21303da3044dae70655c07aa
Angus-Morton <angus.morton@phs.scot>
2025-05-23 17:36:52 +0000

    Create script for cfsd request

---
+++ code/analysis/cfsd_request.R
@@ -0,0 +1,11 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# cfsd_request.R
+# Angus Morton
+# 2025-05-23
+# 
+# Analysis for the outputs requested by cfsd
+# 
+# R version 4.4.2 (2024-10-31)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#### Step 0 : Housekeeping ----
