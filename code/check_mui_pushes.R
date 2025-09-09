f234f4ffdb8e98e7475316a274e6787a67ce150a
rossbu01 <ross.burns3@phs.scot>
2025-09-09 16:31:58 +0000

    Added code for the MUI push check script which generates a list of the non-matching MUIs in excel.

--- code/check_mui_pushes.R
+++ code/check_mui_pushes.R
@@ -11,6 +11,7 @@
 #### Packages ----
 library(readr)
 library(dplyr)
+library(writexl)
 
 #### Edit Filepaths ----
 
@@ -30,7 +31,7 @@ source("code/wait_calculation/all_old_rules.R")
 source("code/wait_calculation/all_new_rules.R")
 
 
-#### Save data ----
+#### Export count of matches ----
 
 # Filter out any records whose wait can't be replicated 
 waits_old_new_wh <- waits_init |> 
@@ -47,6 +48,31 @@ matches <- waits_old_new_wh |>
   pivot_wider(names_from = same_new_wh, values_from = n)
 
 write_csv(matches, paste0("output/", Sys.Date(), "_matches.csv"))
+
+
+#### Export non-matching MUIs ----
+# Save the mui lists for waits that didn't match between the warehouse and new calculation method applied. 
+
+# Get non-matching waits and calculate difference in wait length
+non_matching_new_wh <- waits_old_new_wh |> 
+  mutate(same_old_new = length_all_old_rules == length_all_new_rules,
+         same_new_wh = length_all_new_rules == length_wh,
+         same_old_wh = length_all_old_rules == length_wh) |>
+  filter(!same_new_wh) |>
+  mutate(diff_new_wh = length_wh - length_all_new_rules)
+
+# Get count of wait calculations used in non-matching records
+non_matching_new_wh_summary <- non_matching_new_wh |>
+  group_by(NHS_Board_of_Treatment, Wait_Calculation_Used) |>
+  summarise(n = n())
+
+# Save to output
+write_xlsx(x = list("MUI List" = non_matching_new_wh, 
+                    "Wait Calculations" = non_matching_new_wh_summary), 
+           path = paste0("output/", Sys.Date(), "_non_matching_new_wh.xlsx"))
+
+
+
 
 
 
